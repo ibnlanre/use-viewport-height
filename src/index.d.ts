95f7140b66e40817b4a6dddda7dfbbc6dfc9a05d
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2023-03-08 19:52:11 +0100

    rebuild source

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { CSSProperties, MutableRefObject } from "react";
-type Height = CSSProperties["height"];
-export declare function useViewportHeight<D extends Element>(ref: MutableRefObject<D>): Height;
-export {};
